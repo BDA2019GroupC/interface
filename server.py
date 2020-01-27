b16bd7cb4d2970714ccf62852520bbcd1dfae432
maruyu998 <maruyu998@gmail.com>
2020-01-27 15:35:15 +0900

    up all

--- server.py
+++ server.py
@@ -1,16 +1,35 @@
 from http.server import HTTPServer, BaseHTTPRequestHandler
 from urllib.parse import urlparse
 from urllib.parse import parse_qs
+from urllib.parse import unquote
 import json
+from narouresearch.networks.styleEncoder.model import StyleDisperser
+from narouresearch.conversion.convert import char2ID as char2id, ID2char as id2char
+import torch
 
 PORT = 7604
+
+BOS, EOS, UNK = 0,1,2
+device = torch.device("cuda")
+def transform(batchData):
+    return torch.tensor([[char2id(c) for c in data]+[EOS] for data in batchData]).to(device)
+class models:
+    def __init__(self):
+        print('model loading...')
+        self.model = StyleDisperser(weights=None, method="GRU", input_size=24498, hidden_size=3, output_size=3, device=device, margin=0.2)
+        self.model.to(device)
+    def __call__(self, text):
+        with torch.no_grad():
+            return self.model.inference(transform([text])).cpu()
+
+model = models()
 
 class MyHandler(BaseHTTPRequestHandler):
     def do_POST(self):
         self.make_data()
     def do_GET(self):
         if self.path == '/':
-            self.path = '/web/index.html'
+            self.path = '/interface/index.html'
             try:
                 file_to_open = open(self.path[1:]).read()
                 self.send_response(200)
@@ -19,11 +38,15 @@ class MyHandler(BaseHTTPRequestHandler):
                 self.send_response(404)
             self.end_headers()
             self.wfile.write(bytes(file_to_open, 'utf-8'))
-        else: self.make_data()
+        else:
+            self.make_data()
     def make_data(self):
         parsed = urlparse(self.path)
-        params = parse_qs(parsed.query)
-        body = json.dumps({"cod":[0.22,0.88,0.982]})
+        params = parse_qs(unquote(parsed.query))
+        print(params)
+        ret = model(params['text'])
+        ret = list(ret.cpu().numpy()[0])
+        body = json.dumps({"coord":[str(ret[0]),str(ret[1]),str(ret[2])]})
         self.send_response(200)
         self.send_header('Content-type', 'application/json; charset=utf-8')
         self.send_header('Content-length', len(body.encode()))
